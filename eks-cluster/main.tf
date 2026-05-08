783fae009e38c7a97c6d91525e3c0a289592970e
manu-peter <manup8265@gmail.com>
2026-05-08 12:01:02 +0000

    Added some installation on bastion

--- eks-cluster/main.tf
+++ eks-cluster/main.tf
@@ -6,6 +6,24 @@ data "aws_key_pair" "bastion" {
   key_name = var.key_name
 }
 
+# User data for bastion
+locals {
+  bastion_userdata = <<-EOT
+    #!/bin/bash
+    apt-get update -y
+    curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+    apt-get install unzip -y
+    unzip awscliv2.zip
+    ./aws/install
+    rm -rf aws awscliv2.zip
+    curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+    chmod +x kubectl
+    mv kubectl /usr/local/bin/
+    apt-get install -y tree jq git
+    echo "Bastion setup complete!" > /home/ubuntu/setup-done.txt
+  EOT
+}
+
 # VPC
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
@@ -29,7 +47,6 @@ resource "aws_security_group" "bastion_sg" {
   description = "EKS Bastion host SG"
   vpc_id      = module.vpc.vpc_id
 
-  # SSH ONLY from your IP
   ingress {
     from_port   = 22
     to_port     = 22
@@ -37,7 +54,6 @@ resource "aws_security_group" "bastion_sg" {
     cidr_blocks = ["103.142.31.135/32"]
   }
 
-  # ALL outbound traffic allowed
   egress {
     from_port   = 0
     to_port     = 0
@@ -50,14 +66,15 @@ resource "aws_security_group" "bastion_sg" {
   }
 }
 
-# Bastion EC2 - Ubuntu 22.04 for ap-south-1
+# Bastion EC2
 resource "aws_instance" "bastion" {
-  ami           = "ami-03f4878755434977f" # Ubuntu 22.04 ap-south-1
+  ami           = "ami-03f4878755434977f"
   instance_type = "t3.micro"
   key_name      = data.aws_key_pair.bastion.key_name
   subnet_id     = module.vpc.public_subnets[0]
   vpc_security_group_ids = [aws_security_group.bastion_sg.id]
   associate_public_ip_address = true
+  user_data     = base64encode(local.bastion_userdata)
 
   root_block_device {
     volume_size = 20
@@ -92,4 +109,14 @@ module "eks" {
       key_name       = data.aws_key_pair.bastion.key_name
     }
   }
+}
+
+# Allow bastion to EKS
+resource "aws_security_group_rule" "bastion_to_eks" {
+  type                     = "ingress"
+  from_port                = 0
+  to_port                  = 0
+  protocol                 = "-1"
+  source_security_group_id = aws_security_group.bastion_sg.id
+  security_group_id        = module.eks.cluster_security_group_id
 }
